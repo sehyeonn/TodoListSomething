13b3c811c4454ec4c4774d4d7ddbf732aea94272
sehyeonn <a3920679@naver.com>
2022-02-07 22:16:47 +0900

    complete base, need to CSS design

---
+++ scripts/quotes.js
@@ -0,0 +1,57 @@
+const quotes = [
+    {
+        quote: "You only live once, but if you do it right, once is enough.",
+        author: "Mae West"
+    },
+    {
+        quote: "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+        author: "Maya Angelou"
+    },
+    {
+        quote: "Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.",
+        author: "Bill Keane"
+    },
+    {
+        quote: "I have not failed. I've just found 10,000 ways that won't work.",
+        author: "Thomas A. Edison"
+    },
+    {
+        quote: "If you don't stand for something you will fall for anything.",
+        author: "Gordon A. Eadie"
+    },
+    {
+        quote: "Love all, trust a few, do wrong to none.",
+        author: "William Shakespeare, All's Well That Ends Well"
+    },
+    {
+        quote: "It is never too late to be what you might have been.",
+        author: "George Eliot"
+    },
+    {
+        quote: "For every minute you are angry you lose sixty seconds of happiness.",
+        author: "Ralph Waldo Emerson"
+    },
+    {
+        quote: "If you judge people, you have no time to love them.",
+        author: "Mother Teresa"
+    },
+    {
+        quote: "If you can't explain it to a six year old, you don't understand it yourself.",
+        author: "Albert Einstein"
+    },
+    {
+        quote: "We don't see things as they are, we see them as we are.",
+        author: "Anaïs Nin"
+    },
+    {
+        quote: "Sometimes the questions are complicated and the answers are simple.",
+        author: "Dr. Seuss"
+    },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
---
+++ scripts/weather.js
@@ -0,0 +1,22 @@
+// OpenWeatherMap Api Key here
+const API_KEY = "07b1b0ba183b7f0afca02e9841c37a7a";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
