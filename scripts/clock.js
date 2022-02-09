f6eaf71c9cc32af5feff7489679f2d823146c6a5
sehyeonn <a3920679@naver.com>
2022-02-09 21:40:03 +0900

    Add today's date, add style of input element focus and hover

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,4 +1,7 @@
-const clock = document.querySelector("h1#clock");
+const today = document.querySelector("h2#today");
+const clock = document.querySelector("h2#clock");
+
+const monthes = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 function getClock() {
     const date = new Date();
@@ -7,6 +10,12 @@ function getClock() {
     const seconds = String(date.getSeconds()).padStart(2, "0");
     clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
+
+const date = new Date();
+const year = date.getFullYear();
+const month = monthes[date.getMonth()];
+const day = date.getDate();
+today.innerText = `${month} ${day}, ${year}`
 
 getClock();
 setInterval(getClock, 1000);
