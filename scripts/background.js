904eefdaf14568b0f537764fce402334f7abdc25
sehyeonn <a3920679@naver.com>
2022-02-10 14:27:56 +0900

    Add a Shortcut menu

--- scripts/background.js
+++ scripts/background.js
@@ -16,6 +16,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
+bgImage.id = "background";
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
